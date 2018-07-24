49fdca50ef4dbc423126b38a96fdbd0587e48cfa
Arthur Liberman <arthur_liberman@hotmail.com>
2018-07-24 23:15:58 +0300

    SW I2C improvements Add: Support for slave clock stretching with a timeout. Fix: SDA set to low after ack/nack read. Fix: Stop transmission if slave returned nack.
    Signed-off-by: Arthur Liberman <arthur_liberman@hotmail.com>

--- driver/protocols/i2c.c
+++ driver/protocols/i2c.c
@@ -27,6 +27,7 @@ static void i2c_stop_condition(void);
 static unsigned char i2c_address = 0;
 static unsigned long i2c_delay = I2C_DELAY_100KHz;
 static unsigned char lsb_first = 0;
+static unsigned short clk_stretch_timeout = 0;
 static int pin_scl = 0;
 static int pin_sda = 0;
 
@@ -39,6 +40,7 @@ struct protocol_interface *init_i2c(unsigned char _address, unsigned char _lsb_f
 		pin_scl = _pin_scl;
 		pin_sda = _pin_sda;
 		i2c_delay = _i2c_delay;
+		clk_stretch_timeout = 10 * _i2c_delay;
 		i2c_stop_condition();
 		i2c_ptr = &i2c_interface;
 		pr_dbg2("I2C interface intialized\n");
@@ -64,16 +66,22 @@ static void i2c_stop_condition(void)
 	udelay(i2c_delay);
 }
 
-static unsigned char i2c_ack(void)
+static inline unsigned char i2c_ack(void)
 {
-	unsigned char ret = 1;
+	unsigned char ret = 1, scl = 1;
+	unsigned short timeout = clk_stretch_timeout;
 	gpio_direction_output(pin_scl, LOW);
 	gpio_direction_input(pin_sda);
 	udelay(i2c_delay);
 	gpio_direction_input(pin_scl);
 	udelay(i2c_delay);
+	do {
+		scl = gpio_get_value(pin_scl) ? 1 : 0;
+		udelay(1);
+	} while (!scl && timeout--);
 	ret = gpio_get_value(pin_sda) ? 1 : 0;
 	gpio_direction_output(pin_scl, LOW);
+	gpio_direction_output(pin_sda, LOW);
 	udelay(i2c_delay);
 	return ret;
 }
@@ -134,18 +142,12 @@ static unsigned char i2c_read_cmd_data(const unsigned char *cmd, unsigned int cm
 	i2c_start_condition();
 	if (i2c_address > 0)
 		status = i2c_write_address(i2c_address, 1);
-	if (!status && cmd) {
-		while (cmd_length--) {
-			status |= i2c_write_raw_byte(*cmd);
-			cmd++;
-		}
+	if (cmd) {
+		while (!status && cmd_length--)
+			status |= i2c_write_raw_byte(*cmd++);
 	}
-	if (!status) {
-		while (data_length--) {
-			status |= i2c_read_raw_byte(data);
-			data++;
-		}
-	}
+	while (!status && data_length--)
+		status |= i2c_read_raw_byte(data++);
 	i2c_stop_condition();
 	return status;
 }
@@ -166,18 +168,12 @@ static unsigned char i2c_write_cmd_data(const unsigned char *cmd, unsigned int c
 	i2c_start_condition();
 	if (i2c_address > 0)
 		status = i2c_write_address(i2c_address, 0);
-	if (!status && cmd) {
-		while (cmd_length--) {
-			status |= i2c_write_raw_byte(*cmd);
-			cmd++;
-		}
+	if (cmd) {
+		while (!status && cmd_length--)
+			status |= i2c_write_raw_byte(*cmd++);
 	}
-	if (!status) {
-		while (data_length--) {
-			status |= i2c_write_raw_byte(*data);
-			data++;
-		}
-	}
+	while (!status && data_length--)
+			status |= i2c_write_raw_byte(*data++);
 	i2c_stop_condition();
 	return status;
 }
